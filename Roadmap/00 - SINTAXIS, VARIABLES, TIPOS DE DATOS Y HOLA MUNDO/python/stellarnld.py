f27db1164198a324e37b0fdee322c9bd3f108d79
stellarnld <ccelevrnld@outlook.com>
2025-04-07 17:39:40 -0300

    stellarnld.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/stellarnld.py
@@ -0,0 +1,30 @@
+# https://python.org
+
+# esto es un comentario en una línea
+
+""" 
+Esto también es 
+un comentario pero
+en varias líneas
+"""
+
+´´´ 
+Esto también es 
+un comentario pero
+en varias líneas
+´´´ 
+
+my_variable = "Mi variable" 
+my_variable = "Esta es una nueva variable" 
+
+MY_CONSTANT = "Mi constante" # por convención 
+
+# tipos de variables 
+my_int = 2  # int es un número entero 
+my_float = 0.5 # float es un número con coma 
+my_boolean = True 
+my_boolean = False # los boolean solo pueden ser dos, true o false 
+my_string = "mi cadena de texto" 
+my_other_string = ´mi otra cadena de texto´ # string puede ser un texto, frase, palabra, etc
+
+print("¡Hola, Python!") 
