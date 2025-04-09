c4b9e6c0dd7217b0f1909da4d6bb58a03b9f8981
stellarnld <ccelevrnld@outlook.com>
2025-04-08 23:32:36 -0300

    stellarnld.py
    no esta terminado

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/stellarnld.py
@@ -0,0 +1,89 @@
+# Tipos de operadores
+
+# Operadores aritméticos 
+num1 = 16
+num2 = 200 
+num3 = 105
+num4 = 78 
+
+num2 + num1 # suma 
+
+num3 - num4 # resta 
+
+num1 * num3 # multiplicación 
+
+num4 / num1 # división
+
+num2 % num4 # resto 
+
+num3 // num4 # cociente 
+
+num2 ** num3 # potenciación 
+
+print((num2 - num4) * (num3 / num1) + num4 ** num1) 
+
+# Operadores asignación 
+x = 5 
+
+x += 3  # equivale a x = x + 3 
+8 
+
+x -= 3 
+2 
+
+x *= 3
+15 
+
+x /= 2
+2.5
+
+x %= 2 
+1
+
+x //= 2
+2 
+
+x **= 3
+125
+
+print((x += 3) * (x %= 2))
+
+# Operadores relacionales 
+x = 5 
+
+x == 3 
+False 
+
+x != 7 # ! significa distinto 
+True 
+
+x > 4
+False
+
+x < 9 
+True 
+
+x <= 1
+False 
+
+x >= 5 
+True 
+
+print (x !=  5)
+
+# Operadores lógicos 
+x = False 
+y = True 
+
+x and y # and devuelve True si ambos elementos son True 
+False 
+
+x or y # or devuelve True si al menos un elemento es True 
+True 
+
+not y # not devuelve True si el elemento es falso y viceversa 
+False 
+
+print(x and y) 
+
+
