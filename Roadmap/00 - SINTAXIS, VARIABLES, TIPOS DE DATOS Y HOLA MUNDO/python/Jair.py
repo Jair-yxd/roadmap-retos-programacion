605e0524d708dd100aa3f29c63cd4afafe943748
Jair.yxz <jairnromero@outlook.com.ar>
2025-04-15 18:54:01 -0300

    Jair.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jair.py
@@ -0,0 +1,22 @@
+''' - Debes hacer print por consola del resultado de todos los ejemplos.'''
+
+## -- Arithmethic Operators --
+num1 = 1
+num2 = 2
+
+# --- Additions ---
+
+addition = num1 + num2
+print(f"Addition \n The addition results is: {addition}")
+
+# --- Substraction ---
+substraction = num2 - num1
+print(f"Substraction \n The substraction result is: {substraction}")
+
+# --- Multiplication ---
+multiplication = num1 * num2
+print(f"Multipliation\n The multiplication result is: {multiplication}")
+
+# --- Division ---
+division = int(num2 / num1)
+print(f"Division \n The division result is: {division}")
