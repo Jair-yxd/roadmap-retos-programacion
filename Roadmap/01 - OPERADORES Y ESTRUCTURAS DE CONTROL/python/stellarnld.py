a2d3ba59c74a2b076f4a22ba4303ab36267b8cd7
stellarnld <ccelevrnld@outlook.com>
2025-04-15 17:21:38 -0300

    stellarnld.py

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/stellarnld.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/stellarnld.py
@@ -69,7 +69,7 @@ False
 x >= 5 
 True 
 
-print (x !=  5)
+print(x !=  5)
 
 # Operadores lógicos 
 x = False 
@@ -85,5 +85,49 @@ not y # not devuelve True si el elemento es falso y viceversa
 False 
 
 print(x and y) 
+
+# Operadores especiales 
+# Operador de identidad 
+´´´comparan la identidad de dos objetos, 
+comprueban si ambos objetos hacen referencia 
+a la misma ubicación de memoria´´´
+
+a = [1, 2, 3]
+b = a 
+
+print(a is b) #True 
+
+print(a is not b) #False 
+
+x = 5 
+x = 10 
+
+print(x is y) #False 
+
+print(x is not y) #True 
+
+# Operador de pertenencia 
+´´´se utilizan para comprobar 
+la pertenencia de un valor a una 
+secuencia o conjunto, como,
+cadenas, listas, conjuntos, etc´´´
+
+Text = "Hello, world" 
+
+print("Hello" in text) #True, "Hello" is present in the string
+
+print("hi" in text) #False, "hi" is not present in the string
+
+fruits = ["apple", "banana", "cherry"]
+
+print("grape" not in fruits) #True, "grape" is not present in the list 
+
+print("banana" not in fruits) #False, "banana" is present in the list
+
+
+
+
+
+
 
 
